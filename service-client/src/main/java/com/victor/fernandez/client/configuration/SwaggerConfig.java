c3d719dfb61f13281b89cdc0c5bc6d7c8f395c37
Victor Fernández <victor.fernandez1006@gmail.com>
2023-02-15 13:13:53 -0500

    Conf swagger

--- service-client/src/main/java/com/victor/fernandez/client/configuration/SwaggerConfig.java
+++ service-client/src/main/java/com/victor/fernandez/client/configuration/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket getDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getApiInfo())
-                .select().apis(RequestHandlerSelectors.basePackage("com.victor.fernandez.user.controller"))
+                .select().apis(RequestHandlerSelectors.basePackage("com.victor.fernandez.client.controller"))
                 .paths(PathSelectors.any()).build()
                 .useDefaultResponseMessages(false);
     }
